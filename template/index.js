34ff5c8fb3b239a59a3ba4d451c4cb3fc72a143a
jeff2106 <jeanphilippesara225@gmail.com>
2022-07-22 13:46:17 +0000

    mise à jour des fichier

--- template/index.js
+++ template/index.js
@@ -2,9 +2,8 @@
  * @format
  */
 
-import { AppRegistry,LogBox } from 'react-native'
+import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
-LogBox.ignoreAllLogs();
